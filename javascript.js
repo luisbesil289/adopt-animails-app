75b126bf23093be6afc4cd2e28aa1a1fc9ff2c40
luisbesil289 <l.besil@gmail.com>
2019-05-28 22:16:58 -0300

    .

---
+++ javascript.js
@@ -0,0 +1,32 @@
+var myVariable = 'bob';
+var myNumero = 100.5;
+
+var myBoolVariable = true;
+var myArray = [1, 'bob', 'steve', 10];
+
+var mynull = null;
+
+var otraVariable = {
+    name: 'pepa',
+    age: 24,
+    ejemploPropiedad: 'valorEjemplo',
+    friends: [
+        {
+            name: 'Juan Carlos'
+        },
+        {
+            name: 'Pedro'
+        }
+    ]
+};
+myNumero = 1;
+
+console(myNumero);
+
+if (myNumero>0) {
+console();
+}else if(myNumero<0){
+console();
+}else{
+console();
+}
