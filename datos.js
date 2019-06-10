a1c576925e868e4d6e1eb461c179ce547d60f007
luisbesil289 <l.besil@gmail.com>
2019-06-10 09:26:52 -0300

    datos

---
+++ datos.js
@@ -0,0 +1,125 @@
+const pageWishlist = "wishlist.html";
+const pageAnimal = "animal.html";
+
+let anis = [];
+
+const wishi = [];
+
+const animales = [{
+        foto: "assets/cat_01.jpg",
+        tipo: "cat",
+        nombre: "Penelope",
+        barrio: "Centro",
+        raza: "Persa",
+        sexo: "M",
+        edad: "2",
+        pelo: "Beige",
+        ojos: "Gris",
+        descripcion: "Some quicktitle and make up the bulk of the card's content.",
+        contacto: "Juan Carlos - 094 236 444 - aca@aca.com"
+    },
+    {
+        foto: "assets/dog_01.jpg",
+        tipo: "dog",
+        nombre: "Max",
+        barrio: "cordón",
+        raza: "Foster",
+        sexo: "M",
+        edad: "1",
+        pelo: "marrón",
+        ojos: "negros",
+        descripcion: "Some quicktitle and make up the bulk of the card's content.",
+        contacto: "Juan Carlos - 094 236 444 - aca@aca.com"
+    },
+    {
+        foto: "assets/dog_02.jpg",
+        tipo: "dog",
+        nombre: "Dante",
+        barrio: "Pocitos",
+        raza: "Caniche",
+        sexo: "H",
+        edad: "2",
+        pelo: "Blanco",
+        ojos: "Celestes",
+        descripcion: "Sasfo asdfsd wrgwer afme quicktitle and make up the bulk of the card's content.",
+        contacto: "Juan Carlos - 095 342 666 - ahi@aca.com"
+    },
+    {
+        foto: "assets/dog_08.jpg",
+        tipo: "dog",
+        nombre: "Dante",
+        barrio: "Pocitos",
+        raza: "Caniche",
+        sexo: "H",
+        edad: "2",
+        pelo: "Blanco",
+        ojos: "Celestes",
+        descripcion: "Sasfo asdfsd wrgwer afme quicktitle and make up the bulk of the card's content.",
+        contacto: "Juan Carlos - 095 342 666 - ahi@aca.com"
+    },
+    {
+        foto: "assets/dog_09.jpg",
+        tipo: "dog",
+        nombre: "Dante",
+        barrio: "Pocitos",
+        raza: "Caniche",
+        sexo: "H",
+        edad: "2",
+        pelo: "Blanco",
+        ojos: "Celestes",
+        descripcion: "Sasfo asdfsd wrgwer afme quicktitle and make up the bulk of the card's content.",
+        contacto: "Juan Carlos - 095 342 666 - ahi@aca.com"
+    },
+    {
+        foto: "assets/dog_10.jpg",
+        tipo: "dog",
+        nombre: "Dante",
+        barrio: "Pocitos",
+        raza: "Caniche",
+        sexo: "H",
+        edad: "2",
+        pelo: "Blanco",
+        ojos: "Celestes",
+        descripcion: "Sasfo asdfsd wrgwer afme quicktitle and make up the bulk of the card's content.",
+        contacto: "Juan Carlos - 095 342 666 - ahi@aca.com"
+    },
+    {
+        foto: "assets/dog_11.jpg",
+        tipo: "dog",
+        nombre: "Dante",
+        barrio: "Pocitos",
+        raza: "Caniche",
+        sexo: "H",
+        edad: "2",
+        pelo: "Blanco",
+        ojos: "Celestes",
+        descripcion: "Sasfo asdfsd wrgwer afme quicktitle and make up the bulk of the card's content.",
+        contacto: "Juan Carlos - 095 342 666 - ahi@aca.com"
+    },
+    {
+        foto: "assets/dog_06.jpg",
+        tipo: "dog",
+        nombre: "Perrote",
+        barrio: "Pocitos",
+        raza: "Caniche",
+        sexo: "H",
+        edad: "2",
+        pelo: "Blanco",
+        ojos: "Celestes",
+        descripcion: "Sasfo asdfsd wrgwer afme quicktitle and make up the bulk of the card's content.",
+        contacto: "Juan Carlos - 095 342 666 - ahi@aca.com"
+    },
+    {
+        foto: "assets/cat_03.jpg",
+        tipo: "cat",
+        nombre: "Gatuno",
+        barrio: "Ciudad Vieja",
+        raza: "Caniche",
+        sexo: "M",
+        edad: "2",
+        pelo: "Blanco",
+        ojos: "Celestes",
+        descripcion: "Sasfo asdfsd wrgwer afme quicktitle and make up the bulk of the card's content.",
+        contacto: "Juan Carlos - 095 342 666 - ahi@aca.com"
+    }
+];
